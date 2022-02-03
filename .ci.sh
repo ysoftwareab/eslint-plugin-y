4a03923b2aa31fe92a165aee947b1737142c321a
Andrei Neculau <andrei.neculau@gmail.com>
2022-02-03 14:08:47 +0100

    fixup! fixup! fixup! fixup! support-firecloud -> yplatform

--- .ci.sh
+++ .ci.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-SUPPORT_FIRECLOUD_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/support-firecloud" && pwd)"
-[[ -e ${SUPPORT_FIRECLOUD_DIR}/Makefile ]] || \
-    git submodule update --init --recursive ${SUPPORT_FIRECLOUD_DIR}
+YP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/yplatform" && pwd)"
+[[ -e ${YP_DIR}/Makefile ]] || \
+    git submodule update --init --recursive ${YP_DIR}
 # shellcheck disable=SC1091
-source ${SUPPORT_FIRECLOUD_DIR}/sh/common.inc.sh
+source ${YP_DIR}/sh/common.inc.sh
 
 # function ci_run_<step>() {
 # }
 
 # shellcheck disable=SC1091
-source "${SUPPORT_FIRECLOUD_DIR}/repo/dot.ci.sh.sf"
+source "${YP_DIR}/repo/dot.ci.sh.yp"
