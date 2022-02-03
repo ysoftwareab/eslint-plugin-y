c5c97b03c1800709afece2dc5346aacf540fe503
Andrei Neculau <andrei.neculau@gmail.com>
2022-02-03 01:44:44 +0100

    whitespace

--- configs/index.js
+++ configs/index.js
@@ -1,142 +1,142 @@
 module.exports = {
-
+  
   'async-await': {
     extends: [
       './async-await.js'
     ]
   },
-
+  
   'babel': {
     extends: [
       './babel.js'
     ]
   },
-
+  
   'basic': {
     extends: [
       './basic.js'
     ]
   },
-
+  
   'browser-only': {
     extends: [
       './browser-only.js'
     ]
   },
-
+  
   'browser': {
     extends: [
       './browser.js'
     ]
   },
-
+  
   'eslint-all': {
     extends: [
       './eslint-all.js'
     ]
   },
-
+  
   'eslint-comments': {
     extends: [
       './eslint-comments.js'
     ]
   },
-
+  
   'fp': {
     extends: [
       './fp.js'
     ]
   },
-
+  
   'import': {
     extends: [
       './import.js'
     ]
   },
-
+  
   'jasmine': {
     extends: [
       './jasmine.js'
     ]
   },
-
+  
   'jest': {
     extends: [
       './jest.js'
     ]
   },
-
+  
   'jsdoc': {
     extends: [
       './jsdoc.js'
     ]
   },
-
+  
   'lodash': {
     extends: [
       './lodash.js'
     ]
   },
-
+  
   'mocha': {
     extends: [
       './mocha.js'
     ]
   },
-
+  
   'no-null': {
     extends: [
       './no-null.js'
     ]
   },
-
+  
   'node-only': {
     extends: [
       './node-only.js'
     ]
   },
-
+  
   'node': {
     extends: [
       './node.js'
     ]
   },
-
+  
   'proper-arrows': {
     extends: [
       './proper-arrows.js'
     ]
   },
-
+  
   'protractor': {
     extends: [
       './protractor.js'
     ]
   },
-
+  
   'typescript-eslint-recommended': {
     extends: [
       './typescript-eslint-recommended.js'
     ]
   },
-
+  
   'typescript': {
     extends: [
       './typescript.js'
     ]
   },
-
+  
   'vue': {
     extends: [
       './vue.js'
     ]
   },
-
+  
   'y': {
     extends: [
       './y.js'
     ]
   },
-
+  
   '_': undefined
 };
