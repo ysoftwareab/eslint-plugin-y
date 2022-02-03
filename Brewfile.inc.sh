733c6ab0bae760c14268f52ff8a828fd3a9e0cb7
Andrei Neculau <andrei.neculau@gmail.com>
2022-02-03 17:45:45 +0100

    canonical nvm install

--- Brewfile.inc.sh
+++ Brewfile.inc.sh
@@ -7,6 +7,12 @@ source ${YP_DIR}/bootstrap/brew-install-node.inc.sh
 # shellcheck disable=SC1091
 source ${YP_DIR}/bootstrap/brew-install-node.nvm.inc.sh
 
-GITHUB_MATRIX_NODE=${GITHUB_MATRIX_NODE:-node}
-nvm install ${GITHUB_MATRIX_NODE}
-nvm use ${GITHUB_MATRIX_NODE}
+[[ -n "${GITHUB_MATRIX_NODE}" ]] || [[ -f .nvmrc ]] || GITHUB_MATRIX_NODE=node
+# nvm install --reinstall-packages-from skips 'npm'
+NPM_VSN=$(nvm use system >/dev/null; npm --version)
+nvm install ${GITHUB_MATRIX_NODE} --reinstall-packages-from=current
+(
+    cd ${NVM_DIR}/versions/node/$(node --version)/lib
+    npm install --global-style npm@${NPM_VSN}
+)
+unset NPM_VSN
